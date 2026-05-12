4281feff411d85a19a109ff5a17e2f6da3aacf73
DevVan <149303648+ivanchikyarchick@users.noreply.github.com>
2026-05-12 23:54:04 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -280,10 +280,10 @@ class KaterynaServer:
                 wf.setframerate(16000)
                 wf.writeframes(audio_bytes)
             
-            # АНТИ-ШУМ та ПІДСИЛЕННЯ (AGC) через FFmpeg (ТИМЧАСОВО СПРОЩЕНО)
+            # АНТИ-ШУМ: FFT Noise Reduction + Voice Bandpass (300-3400Hz)
             processed_filename = audio_filename.replace(".wav", "_proc.wav")
-            # Залишаємо тільки легкий фільтр, щоб не псувати голос
-            cmd = ["ffmpeg", "-y", "-i", audio_filename, "-af", "highpass=f=100", processed_filename]
+            # afftdn=nr=10 - приглушує шум на 10дБ, highpass/lowpass виділяють голос
+            cmd = ["ffmpeg", "-y", "-i", audio_filename, "-af", "afftdn=nr=10,highpass=f=300,lowpass=f=3400", processed_filename]
             proc = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.DEVNULL, stderr=asyncio.subprocess.DEVNULL)
             await proc.wait()
             
